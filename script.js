085c8c0655d921d546e688f5a6b2a54cb9671833
Judith L. <judith.lenz@outlook.de>
2024-09-01 10:36:56 +0200

    more style

--- script.js
+++ script.js
@@ -74,36 +74,6 @@ function highlightBottomContainers(currentUrl) {
   });
 }
 
-/**
- * Saves the current page URL in localStorage and then navigates to the specified target URL.
- *
- * @param {string} targetUrl - The URL of the page to navigate to.
- * @returns {void}
- *
- * @example
- * // Saves the current page and navigates to 'legal-notice.html'
- * saveCurrentPageAndNavigate('legal-notice.html');
- */
-function saveCurrentPageAndNavigate(targetUrl) {
-  localStorage.setItem("previousPage", window.location.href);
-  window.location.href = targetUrl;
-}
-
-/**
- * Navigates to the previously stored page URL from localStorage.
- * If no previous page URL is stored, alerts the user that no previous page is available.
- *
- * @returns {void}
- *
- * @example
- * // Navigates back to the previously stored page
- * goBack();
- */
 function goBack() {
-  const previousPage = localStorage.getItem("previousPage");
-  if (previousPage) {
-    window.location.href = previousPage;
-  } else {
-    alert("No previous page stored.");
-  }
+  window.history.back();
 }
