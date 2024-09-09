c5cc87225b2b1841e663aa65e6bb287be5215817
Alessandro Argenziano <1603alessandro@gmail.com>
2024-09-09 18:53:51 +0200

    added html and css for the summary page

--- scripts/summary.js
+++ scripts/summary.js
@@ -1,7 +1,36 @@
+let toDo = document.getElementById("toDoCount");
+let done = document.getElementById("doneCount");
+let inProgress = document.getElementById("inProgressCount");
+let awaitFeedback = document.getElementById("awaitFeedbackCount");
+let urgent = document.getElementById("urgentCount");
+let allTasks = document.getElementById("allTasks");
+
 /**
  * This function initializes the summary html.
  */
 async function summaryInit() {
   includeHTML();
   awaitGenerateInitials();
+  generateCounts();
+}
+
+/**
+ * This function defines and generates the numbers for counting the tasks in the different categories.
+ */
+function generateCounts() {
+  toDo.innerHTML = counter("to-do");
+  done.innerHTML = counter("done");
+  inProgress.innerHTML = counter("in-progress");
+  awaitFeedback.innerHTML = counter("await-feedback");
+  urgent.innerHTML = countUrgentTasks();
+  allTasks.innerHTML = countAllTasks();
+}
+
+/**
+ * This function calculates the number of tasks in a category.
+ * @param {string} category - board category
+ * @returns number of tasks in a category
+ */
+function counter(category) {
+  return tasksArray.filter((task) => task.board_category === category).length;
 }
