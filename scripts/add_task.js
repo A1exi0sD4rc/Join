22eb9ceccb3908cdbb976d723a9a2a98b3076b65
Judith L. <judith.lenz@outlook.de>
2024-09-11 16:57:19 +0200

    try select contacts js

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -52,3 +52,60 @@ function deactivateAll() {
   document.getElementById("boxLow").classList.remove("low_box_active");
   document.getElementById("boxLow").classList.add("aT_set_prio");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Funktion zum Aktivieren und Deaktivieren des Input-Feldes
+
+  function toggleInput(event) {
+    event.stopPropagation(); // Verhindert, dass das Klick-Ereignis auf das Input-Feld propagiert wird
+    const inputField = document.getElementById("aT_select_contacts");
+    const isFocused = document.activeElement === inputField;
+
+    if (isFocused) {
+      deactivateInput();
+    } else {
+      activateInput();
+    }
+  }
+
+  // Aktivieren des Input-Feldes
+  function activateInput() {
+    const inputField = document.getElementById("aT_select_contacts");
+    inputField.classList.add("active");
+    inputField.focus();
+  }
+
+  // Deaktivieren des Input-Feldes und Zurücksetzen der Stile
+  function deactivateInput() {
+    const inputField = document.getElementById("aT_select_contacts");
+    inputField.classList.remove("active");
+    inputField.blur();
+    inputField.placeholder = "Select contacts to assign";
+    inputField.value = "";
+  }
+
+  // Überprüfen, ob außerhalb des Feldes geklickt wurde
+  document.addEventListener("click", function (event) {
+    const inputField = document.getElementById("aT_select_contacts");
+    const arrowContainer = document.querySelector(".drop_down_arrow_container");
+
+    if (
+      !inputField.contains(event.target) &&
+      !arrowContainer.contains(event.target)
+    ) {
+      deactivateInput();
+    }
+  });
+
+  // Event-Listener für das Input-Feld hinzufügen
+  const inputField = document.getElementById("aT_select_contacts");
+  if (inputField) {
+    inputField.addEventListener("click", toggleInput);
+  }
+
+  // Event-Listener für das PfeilDiv hinzufügen
+  const arrowContainer = document.querySelector(".drop_down_arrow_container");
+  if (arrowContainer) {
+    arrowContainer.addEventListener("click", toggleInput);
+  }
+});
