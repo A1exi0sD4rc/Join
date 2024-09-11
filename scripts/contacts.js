debac64e8f923df530cce7cdeed11a8515913c1b
VincentGörner <161941709+A1exi0sD4rc@users.noreply.github.com>
2024-09-11 21:03:21 +0200

    contacts add firebase confi

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -11,6 +11,21 @@ async function init() {
 }
 
 
+async function initFirebase() {
+    const firebaseConfig = {
+        apiKey: "API_KEY",
+        authDomain: "PROJECT_ID.firebaseapp.com",
+        databaseURL: "https://PROJECT_ID.firebaseio.com",
+        projectId: "PROJECT_ID",
+        storageBucket: "PROJECT_ID.appspot.com",
+        messagingSenderId: "SENDER_ID",
+        appId: "APP_ID"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+}
+
+
 async function getContacts() {
     let response = await fetch(BASE_URL + ".json");
     let responseAsJson = await response.json();
