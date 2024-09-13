ed7ee575ff779eddb19fb21e1e5b7e2a0bc331d8
Alessandro Argenziano <1603alessandro@gmail.com>
2024-09-14 01:06:01 +0200

    changed logo animation on login page, adjusted css for the animations

--- scripts/start_img.js
+++ scripts/start_img.js
@@ -1,36 +1,51 @@
 /**
+ * DOM Elements.
+ */
+const startingScreen = document.getElementById("startingScreen");
+const image = document.getElementById("responsiveImage");
+
+/**
  * Updates the image source and background color based on screen width.
  */
 function updateImageSource() {
   const isSmallScreen = window.innerWidth <= 720;
 
-  // Switch image and styles based on screen width
   if (isSmallScreen) {
-    document.body.style.backgroundColor = "#2A3647";
     image.src = "./assets/img/logo_white.svg";
     startingScreen.style.transform = "translate(-50%, -50%)";
   } else {
-    document.body.style.backgroundColor = "#F6F7F8";
     image.src = "./assets/img/logo_black.svg";
     startingScreen.style.transform = "translate(-50%, -50%)";
   }
 }
 
-// Cache DOM elements
-const startingScreen = document.getElementById("startingScreen");
-const image = document.getElementById("responsiveImage");
+/**
+ * Handles the background color transition to match the logo's animation.
+ */
+function triggerBackgroundTransition() {
+  setTimeout(() => {
+    document.body.style.transition = "background-color 1s ease";
+    document.body.style.backgroundColor = "#F6F7F8";
+  }, 500);
+}
 
-// Update image on page load and window resize
-window.addEventListener("resize", updateImageSource);
-window.addEventListener("load", updateImageSource);
+startingScreen.addEventListener("animationstart", function () {
+  if (window.innerWidth <= 720) {
+    triggerBackgroundTransition();
+  }
+});
 
 /**
- * Event handler for the end of the startingScreen animation.
- * Changes the image source and background color if the window width is 720 pixels or less.
+ * Handles when the logo finishes animating, switching the logo source.
  */
 startingScreen.addEventListener("animationend", function () {
   if (window.innerWidth <= 720) {
     image.src = "./assets/img/logo_black.svg";
-    document.body.style.backgroundColor = "#F6F7F8";
   }
 });
+
+/**
+ * Update image on page load and window resize.
+ */
+window.addEventListener("resize", updateImageSource);
+window.addEventListener("load", updateImageSource);
