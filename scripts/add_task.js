75f48494894f253c79b83ea4715bb28075fe069e
Alessandro Argenziano <1603alessandro@gmail.com>
2024-09-16 12:21:11 +0200

    getContacts function in add_task_contacts.js ausgelagert.

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -2,52 +2,6 @@ async function initAddTask() {
   await getContacts();
 }
 
-//fetch Contacts
-
-let CONTACTS_URL =
-  "https://join-a2f86-default-rtdb.europe-west1.firebasedatabase.app/contacts";
-contactsAddTask = [];
-
-async function getContacts() {
-  let response = await fetch(CONTACTS_URL + ".json");
-  let responseAsJson = await response.json();
-
-  try {
-    contactsAddTask = responseAsJson;
-    console.log(contactsAddTask);
-    contactsAddTask.sort((a, b) => a.name.localeCompare(b.name));
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-function getInitials(name) {
-  return name
-    .split(" ")
-    .map((word) => word.charAt(0))
-    .join("");
-}
-
-function renderContacts() {
-  let contactContainer = document.getElementById("contacts_container");
-  contactContainer.innerHTML = "";
-  for (let i = 0; i < contactsAddTask.length; i++) {
-    contactContainer.innerHTML += /*html*/ `
-      <div><svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="21" cy="21" r="20" fill="${
-                              contactsAddTask[i]["bgcolor"]
-                            }" stroke="white" stroke-width="2"/>
-                            <text x="21" y="24" text-anchor="middle" font-size="12" fill="white">${getInitials(
-                              contactsAddTask[i]["name"]
-                            )}</text>
-                        </svg> <div>${
-                          contactsAddTask[i]["name"]
-                        }</div><input type="checkbox">
-      </div>
-    `;
-  }
-}
-
 document.addEventListener("DOMContentLoaded", function () {
   const handle = document.querySelector(".resize-handle");
   const textarea = document.querySelector(".resizable-textarea");
